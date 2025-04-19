5cc6616c63d76b5e530406960eb2b42e1b1fea66
JetVolcano <184006209+JetVolcano@users.noreply.github.com>
2025-04-19 11:16:32 -0700

    Update some code to increase pylint score

--- optimizing/optimizer.py
+++ optimizing/optimizer.py
@@ -1,11 +1,11 @@
 import json
 def optimize(file_path: str, key: str):
-    with open(file_path, "r+") as file:
-        with open('langs.json') as langs:
+    with open(file_path, encoding="r+") as file:
+        with open('langs.json', encoding="r") as langs:
             keys: dict[str, str] = json.load(langs)[keys]
             def get_lang(type: str):
-                type = type.casefold()
-                return keys[type]
+                lang = lang.casefold()
+                return keys[lang]
             try:
                 from google import genai
                 client = genai.Client(api_key=key)
@@ -15,7 +15,7 @@ def optimize(file_path: str, key: str):
                 )
                 filetype = file.name.split('.')[-1]
                 filetype = get_lang(filetype)
-                code = response.text.split(f'```')[1].split('```')[0].split(filetype)[1].strip()
+                code = response.text.split('```')[1].split('```')[0].split(filetype)[1].strip()
                 file.writelines(["\n", "\n", code])
             except Exception as error:
                 raise error
