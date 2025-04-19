8108f9e3fb0825125a93c3c234398da38608412a
JetVolcano <184006209+JetVolcano@users.noreply.github.com>
2025-04-19 11:19:29 -0700

    Refactor optimizer.py to remove unnecessary try-except block and improve readability

--- optimizing/optimizer.py
+++ optimizing/optimizer.py
@@ -1,4 +1,6 @@
 import json
+from google import genai
+
 def optimize(file_path: str, key: str):
     with open(file_path, encoding="r+") as file:
         with open('langs.json', encoding="r") as langs:
@@ -6,16 +8,13 @@ def optimize(file_path: str, key: str):
             def get_lang(lang: str):
                 lang = lang.casefold()
                 return keys[lang]
-            try:
-                from google import genai
-                client = genai.Client(api_key=key)
-                response = client.models.generate_content(
-                    model="gemini-2.5-pro-exp-03-25",
-                    contents = ['optimize my code and output raw code', file.read()]
-                )
-                filetype = file.name.split('.')[-1]
-                filetype = get_lang(filetype)
-                code = response.text.split('```')[1].split('```')[0].split(filetype)[1].strip()
-                file.writelines(["\n", "\n", code])
-            except Exception as error:
-                raise error
+            
+            client = genai.Client(api_key=key)
+            response = client.models.generate_content(
+                model="gemini-2.5-pro-exp-03-25",
+                contents = ['optimize my code and output raw code', file.read()]
+            )
+            filetype = file.name.split('.')[-1]
+            filetype = get_lang(filetype)
+            code = response.text.split('```')[1].split('```')[0].split(filetype)[1].strip()
+            file.writelines(["\n", "\n", code])
